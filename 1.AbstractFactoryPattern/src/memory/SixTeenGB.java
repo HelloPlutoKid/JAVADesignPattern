cc8dcb147a57b1490d9b32a01a11e650aa637775
PlutoKid <tnska52@naver.com>
2019-12-24 01:01:45 +0900

    upload

---
+++ 1.AbstractFactoryPattern/src/memory/SixTeenGB.java
@@ -0,0 +1,14 @@
+package memory;
+
+import Inter.IMemory;
+
+/**
+ * SixTeenGB
+ */
+public class SixTeenGB implements IMemory{
+
+    @Override
+    public void memoryInfo() {
+        System.out.println(" 16 GB Memory");
+    }
+}
